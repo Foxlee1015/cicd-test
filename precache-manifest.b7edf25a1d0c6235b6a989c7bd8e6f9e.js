dc2bf6f9936431f68b603582f16edf4cb51284c2
Foxlee1015 <Foxlee1015@users.noreply.github.com>
2020-05-18 14:14:06 +0000

    deploy: 703511286dc00952ebf49cd80b7cfebdbebb7d39

---
+++ precache-manifest.b7edf25a1d0c6235b6a989c7bd8e6f9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c36e0c127ddefad980cd7bb796a6c8b7",
+    "url": "/cicd-test/index.html"
+  },
+  {
+    "revision": "be5593984ba39945ad9f",
+    "url": "/cicd-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "79e306f3d0f87e77a035",
+    "url": "/cicd-test/static/js/2.729cbb42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cicd-test/static/js/2.729cbb42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be5593984ba39945ad9f",
+    "url": "/cicd-test/static/js/main.28517528.chunk.js"
+  },
+  {
+    "revision": "85874b2a7418c85b3749",
+    "url": "/cicd-test/static/js/runtime-main.c21ce398.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cicd-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
